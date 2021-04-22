72665739b270c8db2a91b52583e98e007fb03885
Vinh Phuc Ta Dang <vinhphuctadang@gmail.com>
2021-04-22 15:30:12 +0700

    add new model

--- image-indexer/Program.cs
+++ image-indexer/Program.cs
@@ -13,12 +13,7 @@ namespace image_indexer
             //Load train data
             NDarray x = np.array(new float[,] { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } });
             NDarray y = np.array(new float[] { 0, 1, 1, 0 });
-
-            //Build sequential model
-            var model = new Sequential();
-            model.Add(new Dense(32, activation: "relu", input_shape: new Shape(2)));
-            model.Add(new Dense(64, activation: "relu"));
-            model.Add(new Dense(1, activation: "sigmoid"));
+            
         }
     }
 }
